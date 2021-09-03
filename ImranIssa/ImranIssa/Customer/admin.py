5ed716f148a4d40d6da31de5d3a02e2b20a57db5
ArifKamal123 <45206957+ArifKamal123@users.noreply.github.com>
2021-09-04 00:40:52 +0500

    Add files via upload

---
+++ ImranIssa/ImranIssa/Customer/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Hotels,Packages,Customer
+# Register your models here.
+
+
+admin.site.register(Hotels)
+
+admin.site.register(Packages)
+
+admin.site.register(Customer)
+
