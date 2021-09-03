4b042c4cf17d22c47c57a36a0d45fa62e841929c
ArifKamal123 <45206957+ArifKamal123@users.noreply.github.com>
2021-09-04 01:20:39 +0500

    Update settings.py

--- ImranIssa/ImranIssa/settings.py
+++ ImranIssa/ImranIssa/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '^wne9jh@tezz@qoc5^6yyfyup+sf-8t@0c_6n(l04vl_$0g(x%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['iilive.herokuapp.com','127.0.0.1:8000']
 
